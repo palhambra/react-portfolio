bf3de8839dd3e2b8f1e2f350fa8eae54e6e4f249
Paolo <paolo.alhambra@yahoo.com>
2023-09-04 19:47:29 -0700

    Home component

--- portfolio-react-app/src/components/Home.js
+++ portfolio-react-app/src/components/Home.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
+import { HiArrowNarrowRight } from 'react-icons/hi'
 
 const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#26292B]'>
 
-    {/* Container */}
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-[#A2B2EE]'>Hi, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Paolo Alhambra</h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer.</h2>
+        <p className='text-[#8892b0]'>I'm ready to be your next hire</p>
+
+        <div>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#A2B2EE] hover:border-[#A2B2EE]'>View Work
+            <span className='group-hover:rotate-90 duration-300'>
+
+              <HiArrowNarrowRight className='ml-3' />
+            </span>
+          </button>
+        </div>
+      </div>
 
     </div>
   )
