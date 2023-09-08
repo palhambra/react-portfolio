7753063d6f49cb1c7f7d29b8e9a2aa48dfd0457d
Paolo <paolo.alhambra@yahoo.com>
2023-09-07 22:33:09 -0700

    updated skills.js

---
+++ portfolio-react-app/src/components/Skills.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import HTML from '../assets/html.png'
+import CSS from '../assets/css.png'
+import JavaScript from '../assets/javascript.png'
+import ReactImg from '../assets/react.png'
+import Tailwind from '../assets/tailwind.png'
+import GitHub from '../assets/github.png'
+import Mongo from '../assets/mongo.png'
+
+const Skills = () => {
+  return (
+    <div name='skills'>
+      {/* Container */}
+      <div>
+        <p>Experience</p>
+        <p>These are the technologies I've worked with</p>
+      </div>
+    </div>
+
+  )
+}
+
+export default Skills
