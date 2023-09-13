fe7eda06f89531d1408cf46106e3cb096a7988be
Paolo <paolo.alhambra@yahoo.com>
2023-09-12 19:13:06 -0700

    added projects to work.js

--- portfolio-react-app/src/components/Work.js
+++ portfolio-react-app/src/components/Work.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import movingPicture from '../assets/Moving-Pictures.png'
+import sluffinOrBuffin from '../assets/Sluffin-or-Buffin.png'
+import impressYourFriends from '../assets/Impress-Your-Friends.png'
 
 const Work = () => {
   return (
@@ -9,23 +12,73 @@ const Work = () => {
           <p className='py-6'>Check out some of my recent work</p>
         </div>
 
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-          <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-            <div>
-              <span>
+        <div
+          className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+          {/* Cards */}
+          <div style={{ backgroundImage: `url(${sluffinOrBuffin})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Fitness Blog Site
 
               </span>
-              <div>
+              <div className='pt-8 text-center'>
                 <a href='/'>
-                  <button>Code</button>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                 </a>
                 <a href='/'>
-                  <button>Demo</button>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Cards */}
+          <div style={{ backgroundImage: `url(${movingPicture})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Instrument Buy/Sell
+
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Cards */}
+          <div style={{ backgroundImage: `url(${impressYourFriends})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Food/Drink Generator
+
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                 </a>
               </div>
             </div>
           </div>
         </div>
+        
       </div>
     </div>
   )
