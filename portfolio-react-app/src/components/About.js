2c7ad0e838e826095d720f0deb162fe7fdbf24ec
Paolo <paolo.alhambra@yahoo.com>
2023-09-16 14:59:20 -0700

    Icon links working

--- portfolio-react-app/src/components/About.js
+++ portfolio-react-app/src/components/About.js
@@ -18,6 +18,9 @@ const About = () => {
             </div>
             <div>
               <p>I've always been interested in coding, but never had the courage to change careers. However, with some support, I've taken a bootcamp to try and better myself. I'm always down to learn new things no matter how difficult it may seem.</p>
+              {/* <p>I've always had this curiosity about coding, you know? But I never quite had the guts to switch careers and dive into it headfirst. With some encouragement and a bit of a nudge, I decided to take the leap and enroll in a coding bootcamp. Learning new things has always excited me, no matter how tough they may seem. 
+
+I come from a background as an automotive technician, specializing in diagnosis. And now, I'm eager to bring the problem-solving and diagnostic skills I've honed in the world of cars into the realm of software development. So here I am, ready to embrace this journey and see where it takes me!</p> */}
             </div>
           </div>
         </div>
--- portfolio-react-app/src/components/Navbar.js
+++ portfolio-react-app/src/components/Navbar.js
@@ -19,27 +19,27 @@ const Navbar = () => {
 
       {/* Menu */}
       <ul className='hidden md:flex'>
-        <li>
+        <li className='hover:border-b-2 hover:border-[#5F7ABD]'>
         <Link to="home" smooth={true} duration={500}>
           Home
         </Link>
         </li>
-        <li>
+        <li className='hover:border-b-2 hover:border-[#5F7ABD]'>
         <Link to="about" smooth={true} duration={500}>
           About
         </Link>
         </li>
-        <li>
+        <li className='hover:border-b-2 hover:border-[#5F7ABD]'>
         <Link to="skills" smooth={true} duration={500}>
           Skills
         </Link>
         </li>
-        <li>
+        <li className='hover:border-b-2 hover:border-[#5F7ABD]'>
         <Link to="work" smooth={true} duration={500}>
           Work
         </Link>
         </li>
-        <li>
+        <li className='hover:border-b-2 hover:border-[#5F7ABD]'>
         <Link to="contact" smooth={true} duration={500}>
           Contact
         </Link>
@@ -53,27 +53,27 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#26292B] flex flex-col justify-center items-center'}>
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-4xl hover:border-b-2 hover:border-[#5F7ABD]'>
         <Link onClick={handleClick} to="home" smooth={true} duration={500}>
           Home
         </Link>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-4xl hover:border-b-2 hover:border-[#5F7ABD]'>
         <Link onClick={handleClick} to="about" smooth={true} duration={500}>
           About
         </Link>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-4xl hover:border-b-2 hover:border-[#5F7ABD]'>
         <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
           Skills
         </Link>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-4xl hover:border-b-2 hover:border-[#5F7ABD]'>
         <Link onClick={handleClick} to="work" smooth={true} duration={500}>
           Work
         </Link>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-4xl hover:border-b-2 hover:border-[#5F7ABD]'>
         <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
           Contact
         </Link>
@@ -85,20 +85,23 @@ const Navbar = () => {
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0072b1]'>
             <a className='flex justify-between items-center w-full text-gray-300' 
-            href='/'>
+            href='https://www.linkedin.com/in/paolo-alhambra-438069284/' target='blank' rel='noopener noreferrer'>
               LinkedIn <FaLinkedin size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333]'>
             <a className='flex justify-between items-center w-full text-gray-300' 
-            href='/'>
+            href='https://github.com/palhambra' target='blank' rel='noopener noreferrer'>
               GitHub <FaGithub size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#720e9e]'>
             <a className='flex justify-between items-center w-full text-gray-300' 
             href='/'>
-              Email <HiOutlineMail size={30} />
+          <Link to="contact" smooth={true} duration={500}>
+              Email 
+            </Link>
+            <HiOutlineMail size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#122543]'>
